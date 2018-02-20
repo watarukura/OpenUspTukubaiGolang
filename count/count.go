46cf199f4d3dcdc06234ade22a0a527690ff86ba
wataru-kurashima <wataru-kurashima@hands-lab.com>
2018-02-20 13:31:58 +0900

    init count.go

---
+++ count/count.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"unicode/utf8"
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `
+Usage of %s:
+   %s <startKeyFieldNumber> <endKeyFieldNumber> [<inputFileName>]
+`, filepath.Base(os.Args[0]), filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+	param := flag.Args()
+	// debug: fmt.Println(param)
+
+	startkeyFldNum, endKeyFldNum, records := validateParam(param)
+	// validateParam(param)
+
+	count(startkeyFldNum, endKeyFldNum, records)
+}
+
+func fatal(err error) {
+	_, fn, line, _ := runtime.Caller(1)
+	fmt.Fprintf(os.Stderr, "%s %s:%d %s ", os.Args[0], fn, line, err)
+	os.Exit(1)
+}
+
+func validateParam(param []string) (starKeyFldNum int, endKeyFldNum int, records [][]string) {
+	var start string
+	var end string
+	var file string
+	var reader io.Reader
+	var err error
+	switch len(param) {
+	case 2:
+		start, end = param[0], param[1]
+		reader = bufio.NewReader(os.Stdin)
+	case 3:
+		start, end, file = param[0], param[1], param[2]
+		reader, err = os.OpenFile(file, os.O_RDONLY, 0600)
+		if err != nil {
+			fatal(err)
+		}
+	default:
+		fatal(errors.New("failed to read param"))
+	}
+
+	starKeyFldNum, err = strconv.Atoi(start)
+	if err != nil {
+		fatal(err)
+	}
+
+	endKeyFldNum, err = strconv.Atoi(end)
+	if err != nil {
+		fatal(err)
+	}
+
+	csvr := csv.NewReader(reader)
+	delm, _ := utf8.DecodeLastRuneInString(" ")
+	csvr.Comma = delm
+	csvr.TrimLeadingSpace = true
+
+	records, err = csvr.ReadAll()
+	if err != nil {
+		fatal(err)
+	}
+
+	return starKeyFldNum, endKeyFldNum, records
+}
+
+func count(startkeyFldNum int, endKeyFldNum int, records [][]string) {
+}
