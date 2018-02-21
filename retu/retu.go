5ea8f53b5faf01cb4ffa748bf7a16099f09e7fcd
watarukura <what.r.j150@gmail.com>
2018-02-22 00:53:28 +0900

    retu.go

---
+++ retu/retu.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `
+Usage of %s:
+   %s [<inputFileName>]
+`, filepath.Base(os.Args[0]), filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+	param := flag.Args()
+	// debug: fmt.Println(param)
+
+	retu(param)
+}
+
+func fatal(err error) {
+	_, fn, line, _ := runtime.Caller(1)
+	fmt.Fprintf(os.Stderr, "%s %s:%d %s ", os.Args[0], fn, line, err)
+	os.Exit(1)
+}
+
+func retu(param []string) {
+	var file io.Reader
+	var err error
+	if len(param) != 0 {
+		if len(param) != 1 {
+			fatal(errors.New("failed to read param"))
+		}
+		file, err = os.Open(param[0])
+		if err != nil {
+			fatal(err)
+		}
+	} else {
+		file = os.Stdin
+	}
+	scanner := bufio.NewScanner(file)
+	nf := 0
+	prev := 0
+	for scanner.Scan() {
+		nf = len(strings.Fields(scanner.Text()))
+		if nf != prev {
+			fmt.Println(nf)
+		}
+		prev = nf
+	}
+}
